427629e77563b06ca135fb298cc0c3979f31acd7
ghufyoo <ghuf.yoo@gmail.com>
2022-05-11 13:57:42 +0800

    ghufyoo

---
+++ FlashWebsite/assets/js/login.js
@@ -0,0 +1,16 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('containers');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+function back() {
+	document.location.href = 'index.html'
+
+
+}
